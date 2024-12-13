9a6a09c4ef2df58a143b323b3903a983da3ca5a7
hwangTaehyeon <clowns1232@gmail.com>
2024-12-13 18:22:08 +0900

    feat: 책 페이징 및 신간 처리

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,8 +2,35 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-  const sql = "SELECT * FROM books";
-  conn.query(sql, (err, results) => {
+  let { category_id, isNew, currentPage, limit } = req.query;
+  if (currentPage && limit) {
+    return res.status(StatusCodes.BAD_REQUEST).end();
+  }
+
+  limit = parseInt(limit);
+  category_id = parseInt(category_id);
+
+  const offset = (currentPage - 1) * limit;
+
+  let sql = "SELECT * FROM books LIMIT ? OFFSET ?";
+  const values = [limit, offset];
+
+  if (category_id && isNew) {
+    sql +=
+      " WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values.push(category_id, isNew);
+  } else if (category_id) {
+    sql += " WHERE category_id=?";
+    values.push(category_id);
+  } else if (isNew) {
+    sql +=
+      " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values.push(isNew);
+  }
+
+  console.log(sql, values);
+
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
@@ -11,36 +38,31 @@ const allBooks = (req, res) => {
     return res.status(StatusCodes.CREATED).json(results);
   });
 };
+
 const bookDetail = (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
 
-  const sql = "SELECT * FROM books WHERE id=?";
+  console.log(id);
+
+  const sql = `SELECT books.*, category.id as category_id, category.category_name FROM books LEFT JOIN category
+   ON books.category_id = category.id WHERE books.id=?`;
+
   conn.query(sql, id, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
-    return res.status(StatusCodes.CREATED).json(results);
-  });
-};
-const booksByCategory = (req, res, next) => {
-  let { category_id } = req.query;
-  if (category_id === undefined) next();
-  category_id = parseInt(category_id);
 
-  const sql = "SELECT * FROM books WHERE category_id=?";
-  conn.query(sql, category_id, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
+    if (results[0]) {
+      return res.status(StatusCodes.CREATED).json(results[0]);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
     }
-    return res.status(StatusCodes.CREATED).json(results);
   });
 };
 
 module.exports = {
   allBooks,
   bookDetail,
-  booksByCategory,
 };
--- routers/books.js
+++ routers/books.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const {
-  allBooks,
-  bookDetail,
-  booksByCategory,
-} = require("../controller/BookController");
+const { allBooks, bookDetail } = require("../controller/BookController");
 
 const router = express.Router();
 
 router.use(express.Router());
 
 // 카테고리별 도서 목록 조회
-router.get("/", booksByCategory);
+router.get("/", allBooks);
 // 전체 도서 조회
 router.get("/", allBooks);
 // 개별 도서 조회
